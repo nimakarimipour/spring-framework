4140195f0026480e9ed430c3ffa79c98bfa2ae52
nimakarimipour <karimipour.nima@gmail.com>
2024-10-18 09:01:17 -0700

    base

---
+++ annotator-command.sh
@@ -0,0 +1,1 @@
+./gradlew clean spring-core:compileJava --rerun-tasks --no-build-cache
--- make_annotator_dirs.py
+++ make_annotator_dirs.py
@@ -1,25 +1,28 @@
 import os
 
+# root
+ROOT = os.getcwd()
+
 nullaway_config = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <serialization>
 	<suggest active="true" enclosing="true"/>
 	<fieldInitInfo active="true"/>
-	<path>/home/nima/Developer/nullness-benchmarks/spring-framework/{}/annotator-out/0</path>
+	<path>{}/{}/annotator-out/0</path>
 	<uuid>3db91ec7-19a7-4a55-bbdf-22b3722ff912</uuid>
 </serialization>
-"""
+""".format(ROOT, {})
 
 scanner_config = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <scanner>
 	<serialization active="true"/>
 	<uuid>eaa892b1-9c85-4193-ae16-30fef023db49</uuid>
-	<path>//home/nima/Developer/nullness-benchmarks/spring-framework/{}/annotator-out/0</path>
+	<path>{}/{}/annotator-out/0</path>
 	<processor>
 		<LOMBOK active="true"/>
 	</processor>
 	<annotations/>
 </scanner>
-"""
+""".format(ROOT, {})
 
 current_dir = os.getcwd()
 for module in os.listdir(current_dir):
