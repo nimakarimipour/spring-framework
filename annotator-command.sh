1fb02a9150e58730cae487b1fd51daf46a28376c
nimakarimipour <karimipour.nima@gmail.com>
2024-10-27 01:47:35 -0700

    prepare more

--- annotator-command.sh
+++ annotator-command.sh
@@ -1,1 +1,6 @@
-./gradlew spring-expression:compileJava
+rm -f nullaway_error_*.txt
+uuid=$(uuidgen)
+./gradlew spring-expression:compileJava 2> error_temp.txt
+head -n 4 error_temp.txt > nullaway_error_${uuid}.txt
+grep -E '[0-9]+ errors' error_temp.txt | tail -n 1
+rm error_temp.txt
